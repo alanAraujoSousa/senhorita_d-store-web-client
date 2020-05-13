ec9f7edbd8d110d956d242b81c9f3b3a68293c84
alan <alan.araujosb@gmail.com>
2020-05-13 12:58:46 -0300

    Create supposed basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ThankyouComponent } from './components/thankyou/thankyou.component';
+import { CartComponent } from './components/cart/cart.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CheckoutComponent,
+    HomeComponent,
+    ProductsComponent,
+    ThankyouComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
