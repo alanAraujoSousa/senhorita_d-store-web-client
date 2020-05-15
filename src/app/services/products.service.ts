2a931f26ef11374b54e1945b52f80ef0732ed50c
alan <alan.araujosb@gmail.com>
2020-05-14 21:31:34 -0300

    change product service to query a single entity

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment'
+import { ProductModelServer } from '../models/product.model';
+import { Observable } from 'rxjs';
+import { ServerResponse } from 'http';
 
 @Injectable({
   providedIn: 'root'
@@ -11,11 +14,19 @@ export class ProductsService {
 
   constructor(private http: HttpClient) { }
 
-  getAllProducts(numberOfResults = 10) {
-    return this.http.get(this.SERVER_URL + '/products', {
+  getAllProducts(numberOfResults = 10): Observable<ServerResponse> {
+    return this.http.get<ServerResponse>(this.SERVER_URL + '/products', {
       params: {
         limit: numberOfResults.toString()
       }
     });
+  }
+
+  getProduct(id: number): Observable<ProductModelServer> {
+    return this.http.get<ProductModelServer>(this.SERVER_URL + '/products/' + id);
+  }
+
+  getProductFromCategory(catName: string): Observable<ProductModelServer[]> {
+    return this.http.get<ProductModelServer[]>(this.SERVER_URL + '/products/category/' + catName);
   }
 }
