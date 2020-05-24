9341167595feadb3d73ee362e0310bdb1066ad67
alan <alan.araujosb@gmail.com>
2020-05-24 14:43:42 -0300

    setup login redirect

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CartModelServer } from '../../models/cart.model';
 import { CartService } from 'src/app/services/cart.service';
+import { UserService } from 'src/app/services/user.service';
+
 
 @Component({
   selector: 'app-header',
@@ -11,12 +13,15 @@ export class HeaderComponent implements OnInit {
 
   cartData: CartModelServer;
   cartTotal: number;
-
-  constructor(public cartService: CartService) { }
+  authState: boolean;
+  
+  constructor(public cartService: CartService,
+              private userService: UserService) { }
 
   ngOnInit(): void {
     this.cartService.cartTotal$.subscribe(total => this.cartTotal = total);
     this.cartService.cartData$.subscribe(data => this.cartData = data);
+    this.userService.authState$.subscribe(authState => this.authState = authState);
   }
 
 }
