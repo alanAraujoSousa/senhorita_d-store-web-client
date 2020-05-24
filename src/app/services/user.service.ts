13e36b63fad53d046c169520ec827954ba6c9c6a
alan <alan.araujosb@gmail.com>
2020-05-24 13:57:41 -0300

    Create user service functionality

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,62 @@
 import { Injectable } from '@angular/core';
+import { AuthService, SocialUser, GoogleLoginProvider } from 'angularx-social-login';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { BehaviorSubject } from 'rxjs';
+import { stringify } from 'querystring';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  auth: boolean = false;
+  private SERVER_URL = environment.SERVER_URL;
+  private user;
+  authState$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(this.auth);
+  userData$: BehaviorSubject<SocialUser | ResponseModel> = new BehaviorSubject<SocialUser | ResponseModel>(null);
+
+  constructor(private authService: AuthService,
+              private httpClient: HttpClient) {
+    
+    authService.authState.subscribe((user: SocialUser) => {
+      if (user != null) {
+        this.auth = true;
+        this.authState$.next(this.auth);
+        this.userData$.next(user);
+      }
+    });
+  }
+  
+  // login user with enaum and passsword
+  loginUser(email: string, password: string) {
+    this.httpClient.post(`${this.SERVER_URL}/auth/login`, { email, password })
+      .subscribe((data: ResponseModel) => {
+        this.auth = data.auth;
+        this.authState$.next(this.auth);
+        this.userData$.next(data);
+      })
+  }
+
+  // Google Authentication
+  googleLogin() {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+  logout() {
+    this.authService.signOut();
+    this.auth = false;
+    this.authState$.next(this.auth);
+  }
+}
+
+interface ResponseModel {
+  token: string;
+  auth: boolean;
+  email: string;
+  username: string;
+  fname: string;
+  lname: string;
+  photoUrl: string;
+  userId: number;
 }
