1b56d4cc21a78605695f1723005966e635bb1003
alan <alan.araujosb@gmail.com>
2020-05-24 20:51:53 -0300

    fix import on product service

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment'
-import { ProductModelServer } from '../models/product.model';
+import { ProductModelServer, ServerResponse } from '../models/product.model';
 import { Observable } from 'rxjs';
-import { ServerResponse } from 'http';
 
 @Injectable({
   providedIn: 'root'
